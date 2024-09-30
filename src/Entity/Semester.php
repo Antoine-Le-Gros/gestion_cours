f878766830f4460d1d327398c6a450a90527b693
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-09-30 15:01:04 +0200

    feat: adding constraint on the number attribute

--- src/Entity/Semester.php
+++ src/Entity/Semester.php
@@ -6,6 +6,7 @@ use App\Repository\SemesterRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SemesterRepository::class)]
 class Semester
@@ -16,6 +17,7 @@ class Semester
     private ?int $id = null;
 
     #[ORM\Column]
+    #[Assert\Range(min: 1, max: 6)]
     private ?int $number = null;
 
     /**
