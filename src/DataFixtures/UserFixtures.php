9653d34fa49701687e83ff74686b4354d5edb111
Louis RTG <louis.rattanavong@etud-univ.reims>
2024-09-30 09:27:23 +0200

    feat: add dependencies to user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,13 +4,21 @@ namespace App\DataFixtures;
 
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         UserFactory::createMany(10);
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ExternalHourRecordFixtures::class,
+        ];
     }
 }
