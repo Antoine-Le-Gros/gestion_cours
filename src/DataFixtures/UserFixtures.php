b4b8e07a2c3df0498f12ec0ceef9c76fdd1e6d08
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-10-03 09:45:20 +0200

    feat: create custom User for each roles

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,6 +11,45 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         UserFactory::createMany(10);
+        UserFactory::createOne([
+            'email' => 'agregate@example.com',
+            'roles' => ['ENSEIGNANT_AGRÉGÉ'],
+            'hoursMax' => 500,
+            'firstname' => 'Jean',
+            'lastname' => 'Dupont',
+            'login' => 'jdupont',
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'email' => 'vacataire@example.com',
+            'roles' => ['VACATAIRE'],
+            'hoursMax' => 150,
+            'firstname' => 'Marie',
+            'lastname' => 'Durand',
+            'login' => 'mdurand',
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'email' => 'chercheur@example.com',
+            'roles' => ['ENSEIGNANT_CHERCHEUR'],
+            'hoursMax' => 250,
+            'firstname' => 'Pierre',
+            'lastname' => 'Martin',
+            'login' => 'pmartin',
+            'password' => 'test',
+        ]);
+
+        UserFactory::createOne([
+            'email' => 'admin@example.com',
+            'roles' => ['ADMINISTRATION'],
+            'hoursMax' => 0,
+            'firstname' => 'Pierre',
+            'lastname' => 'Martin',
+            'login' => 'admin',
+            'password' => 'test',
+        ]);
         $manager->flush();
     }
 }
