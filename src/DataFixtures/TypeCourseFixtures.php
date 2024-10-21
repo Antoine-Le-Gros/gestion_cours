60477c80767f735f5bf590f8e7a454a3edf5a6f7
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2024-10-21 11:00:23 +0200

    fix: change the generated data for typeCourse

--- src/DataFixtures/TypeCourseFixtures.php
+++ src/DataFixtures/TypeCourseFixtures.php
@@ -10,7 +10,10 @@ class TypeCourseFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        TypeCourseFactory::createMany(4);
+        $typesCourse = ['CM', 'TD', 'TP', 'TDM'];
+        foreach ($typesCourse as $typeCourse) {
+            TypeCourseFactory::createOne(['name' => $typeCourse]);
+        }
 
         $manager->flush();
     }
