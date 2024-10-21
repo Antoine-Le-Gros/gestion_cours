11eb90b470451e015403de5cfb41c6edf7080203
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-10-21 15:56:27 +0200

    feat: adding password attribute to test the authentification

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,18 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         UserFactory::createMany(4);
         UserFactory::createOne([
             'email' => 'agregate@example.com',
             'roles' => ['ENSEIGNANT_AGRÉGÉ'],
+            'password' => $this->passwordHasher->hashPassword(new User(), 'test'),
             'hoursMax' => 500,
             'firstname' => 'Jean',
             'lastname' => 'Dupont',
@@ -23,6 +33,7 @@ class UserFixtures extends Fixture
         UserFactory::createOne([
             'email' => 'vacataire@example.com',
             'roles' => ['VACATAIRE'],
+            'password' => $this->passwordHasher->hashPassword(new User(), 'test'),
             'hoursMax' => 150,
             'firstname' => 'Marie',
             'lastname' => 'Durand',
@@ -32,6 +43,7 @@ class UserFixtures extends Fixture
         UserFactory::createOne([
             'email' => 'chercheur@example.com',
             'roles' => ['ENSEIGNANT_CHERCHEUR'],
+            'password' => $this->passwordHasher->hashPassword(new User(), 'test'),
             'hoursMax' => 250,
             'firstname' => 'Pierre',
             'lastname' => 'Martin',
@@ -41,6 +53,7 @@ class UserFixtures extends Fixture
         UserFactory::createOne([
             'email' => 'admin@example.com',
             'roles' => ['ADMINISTRATION'],
+            'password' => $this->passwordHasher->hashPassword(new User(), 'test'),
             'hoursMax' => 0,
             'firstname' => 'Pierre',
             'lastname' => 'Martin',
