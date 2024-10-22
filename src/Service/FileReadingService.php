88a19e174dfa416be8f693de1668d867186133b1
Erwan LECOMTE <erwan.lecomte@etudiant.univ-reims.fr>
2024-10-22 11:24:01 +0200

    feat: adding const for columns index

--- src/Service/FileReadingService.php
+++ src/Service/FileReadingService.php
@@ -7,6 +7,10 @@ use PhpOffice\PhpSpreadsheet\Reader\IReader;
 class FileReadingService
 {
     private IReader $reader;
+    public const TAG_COLUMN = 2;
+    public const TYPE_COLUMN = 4;
+    public const GROUP_MAX_NUMBER_COLUMN = 3;
+    public const SAE_SUPPORT_COLUMN = 5;
 
     public function __construct()
     {
