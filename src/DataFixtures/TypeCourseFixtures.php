c6b7f7aceaf0a64559d943a4b7b50c87edb60edd
Erwan LECOMTE <erwan.lecomte@etudiant.univ-reims.fr>
2024-10-22 10:54:17 +0200

    feat: adding group on TypeCourseFixtures

--- src/DataFixtures/TypeCourseFixtures.php
+++ src/DataFixtures/TypeCourseFixtures.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Factory\TypeCourseFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class TypeCourseFixtures extends Fixture
+class TypeCourseFixtures extends Fixture implements FixtureGroupInterface
 {
     public function load(ObjectManager $manager): void
     {
@@ -16,5 +17,12 @@ class TypeCourseFixtures extends Fixture
         }
 
         $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return [
+            'typeCourse',
+        ];
     }
 }
