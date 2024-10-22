0f3fc5f687846b51f8970880b3dbc346acebe077
clav0016 <mathis.claverie@etudiant.univ-reims.fr>
2024-10-22 10:52:15 +0200

    build: composer require symfony/ux-react

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
