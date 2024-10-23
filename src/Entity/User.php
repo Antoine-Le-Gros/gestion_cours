8e208218ade1a979d252139e5fffb6e262db3813
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-10-23 14:36:23 +0200

    fix: removing delete option on User in API

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
@@ -27,9 +26,6 @@ use Symfony\Component\Serializer\Annotation\Groups;
         new Get(),
         new Patch(
             security: "is_granted('ROLE_USER') and object.getUserIdentifier() == user.getUserIdentifier()",
-        ),
-        new Delete(
-            security: "is_granted('ROLE_ADMIN')",
         ),
     ],
     normalizationContext: ['groups' => ['user_read']],
