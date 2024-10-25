f27b51f40a264c428fc58bfa993885b0912a8416
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-10-25 15:13:54 +0200

    feat: adding method start in LoginAuthentificator

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
@@ -66,5 +67,24 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    /**
+     * Override to control what happens when the user hits a secure page
+     * but isn't logged in yet.
+     *
+     * @throws HttpException
+     */
+    public function start(Request $request, ?AuthenticationException $authException = null): RedirectResponse
+    {
+        /** @var string $route current route */
+        $route = $request->get('_route');
+        // API route ?
+        if (str_starts_with($route, '_api_')) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED);
+        }
+        $url = $this->getLoginUrl($request);
+
+        return new RedirectResponse($url);
     }
 }
