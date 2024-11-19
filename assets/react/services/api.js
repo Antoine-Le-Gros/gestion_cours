9fb201fecf60cffd520c6ae48003423d10197ee1
clav0016 <mathis.claverie@etudiant.univ-reims.fr>
2024-11-19 11:10:39 +0100

    feat: new api request to get authenticated user

--- assets/react/services/api.js
+++ assets/react/services/api.js
@@ -5,3 +5,9 @@ export function fetchUsers() {
         response.ok ? response.json() : Promise.resolve(null),
     );
 }
+export function fetchMe() {
+    return fetch(`${BASE_URL}/me`).then((response) =>
+        response.ok ? response.json() : Promise.resolve(null),
+    );
+
+}
