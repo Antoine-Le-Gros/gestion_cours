eb9724a07001ad716b97dbef463f128eb89e2d0f
clav0016 <mathis.claverie@etudiant.univ-reims.fr>
2024-11-20 14:41:37 +0100

    feat: adding component to handle password changing

---
+++ assets/react/components/Molecule/PassWordChangeModal.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+export default function PasswordChangeModal({ onClose, onPasswordChange }) {
+    const [newPassword, setNewPassword] = useState("");
+
+    const handleConfirm = () => {
+        onPasswordChange(newPassword);
+    };
+
+    return (
+        <div
+            className="modal d-block"
+            style={{
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                top: "10%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    {/* Header avec un style adapté */}
+                    <div className="modal-header bg-black text-white">
+                        <h5 className="modal-title">Changer de mot de passe</h5>
+                        <button
+                            type="button"
+                            className="btn-close btn-close-white"
+                            onClick={onClose}
+                        ></button>
+                    </div>
+                    {/* Body avec un fond clair et texte sombre */}
+                    <div className="modal-body bg-light text-dark">
+                        <label htmlFor="newPassword" className="form-label">
+                            Nouveau mot de passe :
+                        </label>
+                        <input
+                            type="password"
+                            id="newPassword"
+                            className="form-control"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                    </div>
+                    {/* Footer adapté */}
+                    <div className="modal-footer bg-light">
+                        <button className="btn btn-secondary" onClick={onClose}>
+                            Annuler
+                        </button>
+                        <button className="btn btn-dark" onClick={handleConfirm}>
+                            Confirmer
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
