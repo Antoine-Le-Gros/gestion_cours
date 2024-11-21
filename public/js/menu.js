e84b8bb2f758fa368fe1ffa610f6edbcd841bc63
Dylan Bonnevie <dylan.bonnevie@etudiant.univ-reims.fr>
2024-11-21 10:10:16 +0100

    fix: fix open/close issue from navigation

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,13 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
+function initializeSidebarToggle() {
     const hamBurger = document.querySelector("#toggle-btn");
     const sidebar = document.querySelector("#sidebar");
 
     if (hamBurger && sidebar) {
-        hamBurger.addEventListener("click", function () {
-            const isExpanded = sidebar.classList.toggle("expand");
-            hamBurger.setAttribute("aria-expanded", isExpanded);
-        });
+        hamBurger.removeEventListener("click", toggleSidebar);
+        hamBurger.addEventListener("click", toggleSidebar);
     } else {
         console.error("Éléments #toggle-btn ou #sidebar introuvables");
     }
-});
+}
+function toggleSidebar() {
+    const sidebar = document.querySelector("#sidebar");
+    const hamBurger = document.querySelector("#toggle-btn");
+
+    if (sidebar && hamBurger) {
+        const isExpanded = sidebar.classList.toggle("expand");
+        hamBurger.setAttribute("aria-expanded", isExpanded);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", initializeSidebarToggle);
+document.addEventListener("turbo:load", initializeSidebarToggle);
