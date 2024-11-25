3f3a786e324f7f70f48b808c72f8a7e8bd881b8d
Erwan LECOMTE <erwan.lecomte@etudiant.univ-reims.fr>
2024-11-25 11:35:31 +0100

    feat: new way to register semesters completion

--- src/Controller/YearController.php
+++ src/Controller/YearController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Year;
+use App\Repository\AffectationRepository;
+use App\Repository\CourseRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,8 +13,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class YearController extends AbstractController
 {
     #[Route('/year/{id}', name: 'app_year_show', requirements: ['id' => '\d+'])]
-    public function index(#[MapEntity(expr: 'repository.findById(id)')] Year $year): Response
+    public function index(#[MapEntity(expr: 'repository.findById(id)')] Year $year, AffectationRepository $aRepository, CourseRepository $cRepository): Response
     {
+        foreach ($year->getSemesters() as $semester) {
+            $affectations = $aRepository->getAllAffectationGroupsTakenBySemester($semester->getId());
+            $groups = $cRepository->getNumberOfGroupsPerSemester($semester->getId());
+            $semester->setCompletion($affectations / $groups);
+        }
+
         return $this->render('year/show.html.twig', ['year' => $year]);
     }
 }
