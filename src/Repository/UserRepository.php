02e2db5d6b32e4b3d8e8bd8725af5f2d2d5eec71
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2024-11-25 11:55:27 +0100

    feat: add database request to find users by search and role

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -69,28 +69,23 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             ->getQuery()
             ->getResult();
     }
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    /**
+     * @return User[]
+     */
+    public function findBySearchAndRole(string $search, ?string $role): array
+    {
+        $query = $this->createQueryBuilder('u')
+            ->where('UPPER(u.lastname) LIKE UPPER(:search)')
+            ->orWhere('UPPER(u.firstname) LIKE UPPER(:search)')
+            ->setParameter('search', '%'.$search.'%');
+
+        if ($role) {
+            $query->andWhere('CONTAINS(TO_JSONB(u.roles), :role) = TRUE')
+                ->setParameter('role', json_encode([$role]));
+        }
+
+        return $query->getQuery()
+        ->getResult();
+    }
 }
