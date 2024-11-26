3d6e86b613a8ee159f6ceb48efc4a7922d3eb0c1
clav0016 <mathis.claverie@etudiant.univ-reims.fr>
2024-11-26 10:41:55 +0100

    fix: fixing teacher/history route

--- src/Controller/ReactController.php
+++ src/Controller/ReactController.php
@@ -13,7 +13,7 @@ class ReactController extends AbstractController
     #[Route('/discover', name: 'app_discover')]
     #[Route('/history/year', name: 'app_history_year')]
     #[Route('/history/teacher', name: 'app_history_teacher')]
-    #[Route('/history/teacher/{id}', name: 'app_history_teacher')]
+    #[Route('/history/teacher/{id}', name: 'app_history_teacher_show')]
     public function index(): Response
     {
         return $this->render('react/index.html.twig', [
