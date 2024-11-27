ca211bf71e53f5812bda4496108e6084c901ae8c
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-11-27 11:50:16 +0100

    feat: create year edit form on the controller

--- src/Controller/YearController.php
+++ src/Controller/YearController.php
@@ -3,24 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Year;
+use App\Form\YearType;
 use App\Repository\AffectationRepository;
 use App\Repository\CourseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class YearController extends AbstractController
 {
     #[Route('/year/{id}', name: 'app_year_show', requirements: ['id' => '\d+'])]
-    public function index(#[MapEntity(expr: 'repository.findById(id)')] Year $year, AffectationRepository $aRepository, CourseRepository $cRepository): Response
+    public function index(#[MapEntity(expr: 'repository.findById(id)')] Year $year, Request $request, EntityManagerInterface $entityManager, AffectationRepository $aRepository, CourseRepository $cRepository): Response
     {
         foreach ($year->getSemesters() as $semester) {
             $affectations = $aRepository->getAllAffectationGroupsTakenBySemester($semester->getId());
             $groups = $cRepository->getNumberOfGroupsPerSemester($semester->getId());
             $semester->setCompletion($affectations / $groups);
         }
+        $form = $this->createForm(YearType::class, $year);
+        $form->handleRequest($request);
 
-        return $this->render('year/show.html.twig', ['year' => $year]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($year);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_year_show', ['id' => $year->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('year/show.html.twig', [
+            'year' => $year,
+            'form' => $form,
+        ]);
     }
 }
