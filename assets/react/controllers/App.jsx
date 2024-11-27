a474f59a1ac951636834be5f98de762c5cf93ed4
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2024-11-27 17:00:26 +0100

    fix: change route for yearList show

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -22,8 +22,8 @@ function App() {
                 <Route path="/history/year">
                     <YearList/>
                 </Route>
-                <Route path="/history/year/:yearId/:semesterId">
-                    {(params) => <HistorySemester params={{id: params.id, semesterId: params.semesterId}} />}
+                <Route path="/history/year/:id">
+                    {(params) => <HistorySemester params={{id: params.id}} />}
                 </Route>
                 <Route path="/discover">
                     <CourseDiscovery/>
